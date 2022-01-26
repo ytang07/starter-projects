0c8527713f9a9db8ea83223ab400f8221b9bb660
Yujian Tang <yujian@Yujians-MacBook-Pro.local>
2022-01-26 08:10:20 -0800

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+text1 = "Humpty Dumpty sat on a"
+text2 = "Humpty Dumpty had a great"
+text3 = "All the king's men and all the king's"
+text4 = "couldn't"
+text5 = "Humpty Dumpty back"
+
+word1 = input(text1+" ")
+word2 = input(text2+" ")
+word3 = input(text3+" ")
+word4 = input(text4+" ")
+word5 = input(text5+" ")
+
+print(text1, word1, text2, word2, text3, word3, text4, word4, text5, word5)
