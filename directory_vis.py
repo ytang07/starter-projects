b701f5437329dc20403f258aef1a710fce05252f
Yujian Tang <yujian@Yujians-MacBook-Pro.local>
2022-08-30 11:07:47 -0700

    adding directory vis program

---
+++ directory_vis.py
@@ -0,0 +1,22 @@
+# visualize a directory
+import os
+
+# produces a list of 3-tuples
+dir = os.walk(".") 
+
+structure = {}
+for p in dir:
+    for entry in p:
+        top_folder = p[0]
+        folders = p[1]
+        files = p[2]
+        structure[top_folder] = {
+            "Folders": folders,
+            "Files": files
+        }
+
+for top_folder, content in structure.items():
+    print(f"Top Folder: {top_folder}")
+    for folder, files in content.items():
+        print(f"    Folder: {folder}")
+        print(f"        Files: {files}")
